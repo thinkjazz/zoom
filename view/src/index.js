8c0aa41898170b079cd9c6125d22d6fd0efcb35b
Bragin Boris <bragin.boris@ya.ru>
2022-04-01 16:52:31 +0300

    Add back and front

---
+++ view/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { ContextProvider } from "./SocketContext";
+
+import './styles.css';
+
+ReactDOM.render(
+    <ContextProvider>
+    <App />
+    </ContextProvider>,
+    document.getElementById('root')
+    );
+
+
+
